428a1e90517b71b62747d620342cf5e05376ece2
David Vargas <dvargas92495@gmail.com>
2022-01-26 22:36:18 -0600

    deploy static stuff s3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const archiver = require("archiver");
 const crypto = require("crypto");
+const mime = require("mime-types");
 const lambda = new AWS.Lambda();
 const appPath = (p) => path.resolve(fs.realpathSync(process.cwd()), p);
 const readDir = (s) =>
@@ -13,12 +14,13 @@ const readDir = (s) =>
           f.isDirectory() ? readDir(`${s}/${f.name}`) : [`${s}/${f.name}`]
         )
     : [];
+const FE_OUT_DIR = path.join(process.env.FE_DIR_PREFIX || "", "public/build");
 
 const options = {
   date: new Date("09-24-1995"),
 };
 
-const deployWithRemix = () => {
+const deployWithRemix = ({ keys, domain = "remix.davidvargas.me" } = {}) => {
   const FunctionName = "remix-davidvargas-me_origin-request";
   const zip = archiver("zip", { gzip: true, zlib: { level: 9 } });
   readDir("out").forEach((f) =>
@@ -61,6 +63,28 @@ const deployWithRemix = () => {
         }
       })
       .then(console.log)
+      .then(() =>
+        Promise.all(
+          (keys
+            ? keys.filter((k) => fs.existsSync(k))
+            : readDir(FE_OUT_DIR)
+          ).map((p) => {
+            const Key = p.substring(FE_OUT_DIR.length + 1);
+            const uploadProps = {
+              Bucket: domain,
+              ContentType: mime.lookup(Key) || undefined,
+            };
+            console.log(`Uploading ${p} to ${Key}...`);
+            return s3
+              .upload({
+                Key,
+                ...uploadProps,
+                Body: fs.createReadStream(p),
+              })
+              .promise();
+          })
+        )
+      )
       .catch((e) => {
         console.error(`deploy failed:`);
         console.error(e);
