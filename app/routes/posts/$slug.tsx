0539fba31b49d57985008794a089728e57701c38
David Vargas <dvargas92495@gmail.com>
2022-01-26 23:58:30 -0600

    try dynamic posts

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,14 +1,15 @@
 import { useLoaderData, LoaderFunction } from "remix";
+import { getPosts } from "~/post";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  return params.slug;
+export const loader = async ({ params }: Parameters<LoaderFunction>[0]) => {
+  return getPosts().find((p) => p.slug === params.slug);
 };
 
 export default function PostSlug() {
-  const slug = useLoaderData();
+  const slug = useLoaderData<Awaited<ReturnType<typeof loader>>>();
   return (
     <div>
-      <h1>Some Post: {slug}</h1>
+      <h1>Some Post: {slug?.title}</h1>
     </div>
   );
 }
